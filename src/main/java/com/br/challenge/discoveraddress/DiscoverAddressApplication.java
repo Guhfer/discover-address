1d9868d46d44f9e03eb749fb9bd09e1d7259e30c
Gustavo <fernandes.guga@hotmail.com>
2021-06-06 11:01:25 -0300

    initial commit

---
+++ src/main/java/com/br/challenge/discoveraddress/DiscoverAddressApplication.java
@@ -0,0 +1,13 @@
+package com.br.challenge.discoveraddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverAddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverAddressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/challenge/discoveraddress/DiscoverAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.challenge.discoveraddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverAddressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
