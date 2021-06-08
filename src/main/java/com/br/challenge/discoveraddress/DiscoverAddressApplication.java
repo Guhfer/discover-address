3597e01b2a3f0f34a72d0ebb10fdcf33853b0bae
Gustavo <fernandes.guga@hotmail.com>
2021-06-08 07:07:48 -0300

    Adicionando configuração do Actuator e Cache

--- src/main/java/com/br/challenge/discoveraddress/DiscoverAddressApplication.java
+++ src/main/java/com/br/challenge/discoveraddress/DiscoverAddressApplication.java
@@ -2,8 +2,10 @@ package com.br.challenge.discoveraddress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DiscoverAddressApplication {
 
     public static void main(String[] args) {
