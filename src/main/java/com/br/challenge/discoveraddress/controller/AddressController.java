c4b228954e26cd8b308304774fd02484e9f516aa
Gustavo <fernandes.guga@hotmail.com>
2021-06-08 07:00:13 -0300

    Alterando service e integracao com servico externo

--- src/main/java/com/br/challenge/discoveraddress/controller/AddressController.java
+++ src/main/java/com/br/challenge/discoveraddress/controller/AddressController.java
@@ -1,18 +1,35 @@
 package com.br.challenge.discoveraddress.controller;
 
+import com.br.challenge.discoveraddress.controller.resource.AddressResponse;
+import com.br.challenge.discoveraddress.service.AddressService;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/addresses")
 public class AddressController {
 
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
 
     @GetMapping("/{cep}")
-    public ResponseEntity getAddressByCep(@PathVariable String cep) {
+    @ApiOperation(value = "Get Address by CEP")
+    public ResponseEntity<AddressResponse> getAddressByCep(@PathVariable String cep) {
 
+        log.info(this.getClass() + " Get address by CEP: " + cep);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(addressService.findAddressByCep(cep));
     }
 }
--- src/main/java/com/br/challenge/discoveraddress/integration/FindAddressIntegration.java
+++ src/main/java/com/br/challenge/discoveraddress/integration/FindAddressIntegration.java
@@ -1,11 +1,41 @@
 package com.br.challenge.discoveraddress.integration;
 
+import com.br.challenge.discoveraddress.controller.resource.AddressCepResource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
+@CacheConfig(cacheNames={"response"})
 public class FindAddressIntegration {
 
-    @Value("${services.viaCep.url}")
-    String baseUrl;
+    private final String baseUrl;
+    private final String pathGet;
+
+    public FindAddressIntegration(@Value("${services.viaCep.url}") String baseUrl,
+                                  @Value("${services.viaCep.path.get}") String pathGet) {
+        this.baseUrl = baseUrl;
+        this.pathGet = pathGet;
+    }
+
+    @Cacheable
+    public AddressCepResource getAddressByCep (String cep) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<AddressCepResource> response = restTemplate.getForEntity(buildUrl(cep), AddressCepResource.class);
+
+        log.info(this.getClass().getName() + " Get address by ViaCep API response: " + response.toString());
+
+        return response.getBody();
+    }
+
+    private String buildUrl (String cep) {
+        return (baseUrl.concat(pathGet)).replace("{cep}", cep);
+    }
+
 }
